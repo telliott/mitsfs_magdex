7721c773852250f8492f8a9cc0bb0d3df244d823
TOBY ELLIOTT <telliott@TOBYs-MacBook-Air.local>
2025-05-04 17:00:12 -0400

    checkin of current progress in cataloguing library, including files for CGI display

---
+++ index.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import os
+import urllib.parse
+import json
+from magdex import magazine
+from magdex import frontpage
+
+# comment this out for the actual site
+import cgitb
+cgitb.enable()
+
+#URL to the main magazine page
+top_url = os.environ["REQUEST_URI"].split('?')[0]
+
+#URL to magazines (tells the frontpage how to construct a magazine link). Actual mag link 
+#will be substituted into the [] sections
+magazine_url = top_url + "?mag=[SECTION]/[MAG]"
+
+# Path to root folder where magazine folders are		
+magazine_root = os.environ["CONTEXT_DOCUMENT_ROOT"] + "/mitsfs_magdex/magazines/"
+
+#query parameters
+params = urllib.parse.parse_qs(os.environ["QUERY_STRING"])
+
+
+print ("Content-type: text/html\r\n\r\n")
+print ("<!DOCTYPE html>\n")
+print (('<html>\n<head>'
+  		'<link rel="stylesheet" href="http://mitsfs.mit.edu/static/css/main.bdbe7b61a7bbc1d899d2d4c72c413f96.css">'
+  		'<style>td {padding: 15px;} th {padding: 15px;}</style>'
+		'</head>\n<body>'))
+
+if "mag" in params:
+
+	#put in a link back to the main page
+	print ("<a href=\"%s\">Back</a>\n" % top_url)
+	(folder, mag) = params["mag"][0].split("/" , 1)
+	
+	print (magazine.createMagazine(folder, mag, magazine_root))
+else:
+	print (frontpage.createFrontpage(magazine_root, magazine_url))
+
+print ("</html>")
---
+++ magdex/frontpage.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python3
+import os
+import re
+import html
+import json
+from math import ceil
+
+SECTIONS = {"library_front": "The Big Guns",
+			"library_small": "Small Magazines",
+			"library_large": "Large Magazines",
+			"library_xl": "Extra-Large Magazines",
+			"annex": "Annex",
+			"annex_foreign": "Annex Foreign Magazines",
+			"library_foreign": "Foreign Magazines",
+			"one_offs.json": "One-Off Issues of Larger Publications (in Annex)"
+			}
+			
+COLUMNS = 4
+
+def loadJson(filename):
+	try:
+		with open(filename) as f:
+			d = json.load(f)
+			return d
+	except (OSError, IOError) as e:
+		return []
+	
+"""
+Loops through the categories and outputs a table for each
+"""
+def createFrontpage(root, link_template):
+	q = "<h1>The MITSFS Magdex</h1>\n"
+	q += "\n<hr>\n".join([tableOfContents(section, root, link_template) for section in SECTIONS.keys()])
+	return q
+	
+"""
+Finds all the filenames in a directory that end with .json and generates a table to reflect them
+"""
+def tableOfContents(section, root, link_template):
+	magazines = []
+	raw_display = section.endswith(".json")
+	if raw_display:
+		magazines = loadJson("/".join([root, section])).get("issues", [])
+	else:
+		directory = root + section
+		magazines = sorted([x for x in os.listdir(directory) if x.endswith(".json")], \
+							key=str.casefold)
+
+	if (not magazines):
+		return "";
+	
+	column_length = ceil(len(magazines) / COLUMNS)
+	columns = [magazines[i:i + column_length] \
+					for i in range(0, len(magazines), column_length)]
+	
+	tagged_columns = []
+	if raw_display:
+		tagged_columns = [outputTextColumn(column) for column in columns]
+	else:	
+		tagged_columns = [outputColumn(section, column, link_template) \
+							for column in columns]
+	
+	q = "<h2>%s</h2>\n" % SECTIONS[section]
+	q += "<table width=\"100%\"><tr>\n"
+	for column in tagged_columns:
+		q+= "<td valign=top>\n%s</td>\n" % column
+	q += "</tr></table>\n"
+	
+
+	return q
+
+"""
+Creates an individual column list of magazines
+"""
+def outputColumn(section, column, link_template):
+	return "<ul>%s</ul>" % "\n".join([create_entry(link_template, section, mag) for mag in column])
+
+"""
+Creates the text for unlinked magazines
+"""
+def outputTextColumn(column):
+	q = ""
+	for mag in column:
+		text = "%s (%s)" % (mag.get("name", ""), mag.get("issue", ""))
+		if mag["note"]:
+			text += " - %s" % mag["note"]
+		if mag["box"]: 
+			text += " (Box %s)" % mag["box"]
+		q += "<li>%s</li>" % text
+	return q
+
+"""
+Strip off the suffix and turn underscores into spaces for the display name.
+Fill in the template URL with the magazine info and return the list item
+"""
+def create_entry(link_template, section, mag):
+	name = re.sub("\..*", "", mag)
+	link = re.sub("\[SECTION\]", html.escape(section), link_template)
+	link = re.sub("\[MAG\]", html.escape(name), link)
+	
+	name = re.sub("_", " ", name)
+
+	return "<li><a href=\"%s\">%s</a></li>" % (link, name.strip())
